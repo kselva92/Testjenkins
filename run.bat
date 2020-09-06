65f3ccc8735a8439452ef622b1071275012854d7
Selva Ganapathi K <selva@symphonyai.com>
2020-09-06 19:28:23 +0530

    changed run script

--- run.bat
+++ run.bat
@@ -1,7 +1,1 @@
-set projectLocation=/var/lib/jenkins/workspace/PHPSmoke
-
-cd %projectLocation%
-
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-
-java org.testng.TestNG %projectLocation%\testng.xml
+java -cp bin;lib/* org.testng.TestNG testng.xml
